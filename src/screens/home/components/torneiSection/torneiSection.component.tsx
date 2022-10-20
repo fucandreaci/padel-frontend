b7adc323a0107a6e00c302b5f68aecf584a14616
Andrea Fucci <afucci@soldo.com>
2022-10-20 16:46:02 +0200

    Alert se non ci sono tornei

--- src/screens/home/components/torneiSection/torneiSection.component.tsx
+++ src/screens/home/components/torneiSection/torneiSection.component.tsx
@@ -1,13 +1,13 @@
 import React, {useEffect} from 'react';
 import './torneiSection.scss'
-import {Typography} from '@mui/material';
+import {Alert, AlertTitle, Typography} from '@mui/material';
 import {useAppDispatch} from 'store/store.config';
 import {torneiAction} from 'store/tornei/tornei.action';
 import {useSelector} from 'react-redux';
 import {torneiSelector} from 'store/tornei/tornei.selector';
 import {TorneoDetail} from './components/torneoDetail/torneoDetail.component';
 
-interface TorneiSectionProps{
+interface TorneiSectionProps {
 }
 
 const componentClassName = 'tornei-section';
@@ -16,24 +16,35 @@ export const TorneiSection = (props: TorneiSectionProps) => {
     const dispatch = useAppDispatch();
 
     const tornei = useSelector(torneiSelector.getTornei)
+    const isLoading = useSelector(torneiSelector.isLoading)
 
     useEffect(() => {
         dispatch(torneiAction.fetchTornei());
     }, []);
 
     return (
-    <div className={`${componentClassName}`}>
-        <Typography component={'p'} variant="h3" align={'center'} gutterBottom>
-            Tornei
-        </Typography>
+        <div className={`${componentClassName}`}>
+            <Typography component={'p'} variant="h3" align={'center'} gutterBottom>
+                Tornei
+            </Typography>
 
-        {
-            tornei.map((torneo) => {
-                return (
-                    <TorneoDetail key={torneo.id} torneo={torneo}/>
+            {
+                tornei.length == 0 && !isLoading && (
+                    <Alert severity="info">
+                        <AlertTitle>Info</AlertTitle>
+                        Non ci sono tornei da mostrare!
+                    </Alert>
                 )
-            })
-        }
-    </div>
+
+            }
+
+            {
+                tornei.map((torneo) => {
+                    return (
+                        <TorneoDetail key={torneo.id} torneo={torneo}/>
+                    )
+                })
+            }
+        </div>
     )
 };
